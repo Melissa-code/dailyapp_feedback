0de9551c14d4f1bc5947f985fa7720ae1d536988
Melissa Benard <benardmeli@yahoo.fr>
2025-08-31 12:00:25 +0200

    feat: add validation for questions 1 and 2 with error messages

--- script.js
+++ script.js
@@ -9,6 +9,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const answers = {
     "q1" : null,  
+    "q2": null,  
+    "q3": []
   }
 
   function getQ1Rating() {
@@ -25,6 +27,19 @@ document.addEventListener("DOMContentLoaded", () => {
     return selected ? selected.value : null;
   }
 
+  function getQ3Features() {
+      // Array.from: NodeList converti en tableau -> map() pour le parcourir
+      const featuresChecked = Array.from(document.querySelectorAll('input[name="feature"]:checked')).map(input => input.value);
+      const otherFeature = document.querySelector('input[name="feature_other"]').value.trim();
+      
+      if (otherFeature) featuresChecked.push(otherFeature);
+
+      console.log("Fonctionnalités sélectionnées:", featuresChecked);
+      console.log("Autre fonctionnalité:", otherFeature);
+
+      return featuresChecked;
+  }
+
   function showCurrentSection() {
     sections.forEach((id, index) => {
         document.getElementById(id).style.display = (index === currentIndex) ? "flex" : "none";
@@ -36,18 +51,55 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll(".next-btn").forEach(btn => {
     btn.addEventListener("click", (e) => {
       e.preventDefault();
+      const errorMessage = document.querySelector(".error-message");
+      errorMessage.textContent = "";
+      errorMessage.style.display = "none";
 
-        if (currentIndex === 1) {
-          answers.q1 = getQ1Rating();
-          console.log(answers);
-        } else if (currentIndex === 2) {
-          answers.q2 = getQ2Recommendation();
-          console.log(answers);
-        } 
+      if (currentIndex === 0) {
+        currentIndex++;
+        showCurrentSection();
+      }
 
+      else if (currentIndex === 1) {
+        answers.q1 = getQ1Rating();
+
+        if (answers.q1) {
+            resultQ1.textContent = answers.q1 + "/5";
+            console.log(answers);
+            currentIndex++;
+            showCurrentSection();
+        } else {
+            const currentSection = document.getElementById("question1");
+            const q1ErrorMessage = currentSection.querySelector(".error-message");
+            q1ErrorMessage.textContent = "Veuillez sélectionner une note avant de continuer.";
+            q1ErrorMessage.style.display = "flex";
+            return; 
+        }
+
+      } else if (currentIndex === 2) {
+        answers.q2 = getQ2Recommendation();
+        if (answers.q2) {
+          resultQ2.textContent = answers.q2 === "yes_q2" ? "Oui" : "Non";
+          console.log(answers);
+          currentIndex++;
+          showCurrentSection();
+        } else {
+          const currentSection = document.getElementById("question2");
+          const q2ErrorMessage = currentSection.querySelector(".error-message");
+          q2ErrorMessage.textContent = "Veuillez sélectionner une réponse avant de continuer.";
+          q2ErrorMessage.style.display = "flex";
+          return;
+        }
+
+      } else if (currentIndex === 3) {
+        answers.q3 = getQ3Features();
+        console.log(answers);
+        
+      } else {
         currentIndex++;
         if (currentIndex >= sections.length) currentIndex = sections.length - 1; 
         showCurrentSection();
+      }
 
     });
   });
