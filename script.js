b6aff04a96e0aafaae9ea93626596e17aea059a3
Melissa Benard <benardmeli@yahoo.fr>
2025-08-31 10:32:22 +0200

    feat: store question1  star rating in answers object

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ document.addEventListener("DOMContentLoaded", () => {
 
   const sections = ["home", "question1", "question2", "question3", "results"];
   let currentIndex = 0; 
+  
+  const resultQ1 = document.querySelector("#result-q1");
+  const resultQ2 = document.querySelector("#result-q2");
+  const resultQ3 = document.querySelector("#result-q3");
+
+  const answers = {
+    "q1" : null,  
+  }
+
+  function getQ1Rating() {
+    const selected = document.querySelector('input[name="q1_rating"]:checked');
+    console.log("Note d'évaluation:", selected ? selected.value + "/5" : "None");
+
+    return selected ? selected.value : null;
+  }
 
   function showCurrentSection() {
     sections.forEach((id, index) => {
@@ -14,9 +29,16 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelectorAll(".next-btn").forEach(btn => {
     btn.addEventListener("click", (e) => {
       e.preventDefault();
+
+        if (currentIndex === 1) {
+          answers.q1 = getQ1Rating();
+          console.log(answers);
+        }
+
         currentIndex++;
         if (currentIndex >= sections.length) currentIndex = sections.length - 1; 
         showCurrentSection();
+
     });
   });
 
